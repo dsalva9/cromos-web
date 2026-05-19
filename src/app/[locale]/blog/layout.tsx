fd4cdc7b12e7fc8d7afc9b5c5ef2308f91973b09
dsalva9 <dsalva@gmail.com>
2026-05-19 14:06:29 +0200

    fix: remove white band at bottom of blog page in dark mode on mobile

--- src/app/[locale]/blog/layout.tsx
+++ src/app/[locale]/blog/layout.tsx
@@ -4,7 +4,7 @@ export default function BlogLayout({
     children: React.ReactNode;
 }) {
     return (
-        <div className="flex flex-col min-h-screen bg-gray-50 dark:bg-gray-900">
+        <div className="flex flex-col flex-1 bg-gray-50 dark:bg-gray-900">
             <main className="flex-1 container mx-auto px-4 py-8 max-w-4xl">
                 {children}
             </main>
